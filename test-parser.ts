3fdf0d9bd3b045d67413f488a2f507b4e7f79dfc
Abhishek <abhishek12318@gmail.com>
2026-03-06 15:22:05 +0530

    xx

---
+++ test-parser.ts
@@ -0,0 +1,20 @@
+enum FinancialEmailTypes {
+    BankTransactionAlerts,
+    CreditCardSpendAlerts,
+    MonthlyBankStatements,
+    MonthlyCreditCardStatements,
+    MutualFundConfirmations,
+    MutualFundCAS,
+    BrokerContractNotes,
+    InsurancePremiums,
+    EMIDeductionAlerts,
+    TaxAndITR,
+    SubscriptionReceipts,
+    SpendingReceipts,
+    UPIAppReceipts,
+}
+
+
+export interface a {
+    "email": ""
+}
